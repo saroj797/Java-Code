45796d70b9b0af31633d47d58cb962860b51155c
saroj797 <sarojraaz234@gmail.com>
2025-08-14 18:49:24 +0530

    yes

---
+++ SimpleCofde.java
@@ -0,0 +1,15 @@
+public class SimpleCofde {
+
+    //saroj
+
+    public static void main(String[] args) {
+        
+        int num1 = 45;
+        int num2 = 45;
+
+        int sum = num1 + num2;
+        
+        System.out.println("Sum of num1 and num2 + " + sum);
+    }
+    
+}
