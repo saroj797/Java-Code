632114a91a67c92de4602f5baa7c77a3a5546618
saroj797 <sarojraaz234@gmail.com>
2025-08-20 22:32:17 +0530

    yes
    yes

---
+++ c laungage/Array.java
@@ -0,0 +1,21 @@
+public class Array{
+
+    public static void main(String args[]){
+    
+    int [] arr = { 10,20,30,40,50};
+        
+        System.out.println(" Print array element:");
+        
+        // using for loop print array
+        
+        for(int i=0; i<5; i++){
+
+        System.out.println( arr[i]);
+
+        }
+
+
+    }
+
+
+}
---
+++ c laungage/first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main ()
+{
+    int a,b,c;
+
+    printf("enter theh value a=");
+    scanf("%d",&a);
+
+    printf("enter the value b=");
+    scanf("%d",&b);
+
+    c= a+b;
+
+    printf(c);
+    return 0;
+
+}
