6e050ebfa830e9f2d380a0e96a46eb094c009f2d
saroj797 <sarojraaz234@gmail.com>
2025-08-26 19:51:37 +0530

    yes

---
+++ c laungage/TwoD.java
@@ -0,0 +1,26 @@
+public class TwoD {
+    
+    public static void main(String arg[]){
+
+        int arr[][] = new int [2][3];
+
+        arr[0][0] = 10;
+        arr[0][1] = 5;
+        arr[0][2] = 6;
+
+        arr[1][1] = 3;
+        arr[1][2] =  4;
+        arr[1][3]  = 7;
+
+        for( int i=0; i = arr.length ; i++){
+
+            for(int j = 0; j = i.length; j--){
+
+            }
+            System.out.println(arr[i][j]);
+
+
+
+        }
+    }
+}
